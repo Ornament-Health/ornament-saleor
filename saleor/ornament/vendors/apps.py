4927848d9ee528c5f7c4dcfb3454321e809ea285
Dmitry Gurinenko <dmitriy.gurinenko@gmail.com>
2023-09-13 23:01:10 +0800

    CORE-2070: fix download_geo_lite_db on docker build

--- saleor/ornament/vendors/apps.py
+++ saleor/ornament/vendors/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
 
-from saleor.ornament.geo.utils import download_geo_lite_db
-
 
 class OrnamentVendorsConfig(AppConfig):
     name = "saleor.ornament.vendors"
-
-    def ready(self) -> None:
-        download_geo_lite_db()
-        return super().ready()
