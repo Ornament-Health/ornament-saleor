3e48f618cbc7cfecb9da9101b4234af7387919ce
Dmitry Gurinenko <dmitriy.gurinenko@gmail.com>
2023-10-25 16:08:56 +0800

    CORE: update SearchProduct query

--- saleor/graphql/ornament/search/resolvers.py
+++ saleor/graphql/ornament/search/resolvers.py
@@ -9,14 +9,28 @@ def resolve_search_products(info):
     qs = (
         ProductChannelListing.objects.filter(visible_in_listings=True)
         .order_by("product_id")
-        .prefetch_related("channel")
+        .prefetch_related("channel", "product__variants__channel_listings")
     )
+
     products = []
+
     for _, group in groupby(qs, attrgetter("product_id")):
         listings = list(group)
         product = listings[0].product
         channels = [listing.channel.slug for listing in listings]
-        # channels = [listing.channel_id for listing in listings]
         product.channels = channels
+
+        # TODO: allow to fetch all variants (all vendors) prices
+        # will be necessary if our app allows to search among different vendors
+        current_variant = product.variants.first()
+        current_variant_prices = current_variant.channel_listings.select_related(
+            "channel"
+        ).values("channel__slug", "price_amount", "currency")
+
+        if not current_variant_prices:
+            continue
+
+        product.current_variant_prices = current_variant_prices
         products.append(product)
+
     return products
--- saleor/graphql/ornament/search/types.py
+++ saleor/graphql/ornament/search/types.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import graphene
 
@@ -12,6 +12,18 @@ from saleor.product import models as product_models
 from saleor.graphql.core.connection import CountableConnection
 
 
+class SearchProductChannelPrice(graphene.ObjectType):
+    channel = graphene.String(description="Price channel")
+    amount = graphene.String(description="Price amount")
+    currency = graphene.String(description="Price currency")
+
+
+class SearchProductType(graphene.ObjectType):
+    id = graphene.GlobalID(required=True, description="The ID of the product type.")
+    name = graphene.String(required=True, description="Name of the product type.")
+    slug = graphene.String(required=True, description="Slug of the product type.")
+
+
 class SearchProduct(ModelObjectType[product_models.Product]):
     id = graphene.GlobalID(required=True)
     channels = graphene.List(
@@ -21,14 +33,19 @@ class SearchProduct(ModelObjectType[product_models.Product]):
     )
     name = graphene.String(required=True, description="Product name.")
     description = JSONString(description="Description of the product.")
-    biomarkers = graphene.Field(
-        graphene.List(graphene.Int),
+    biomarkers = graphene.List(
+        graphene.Int,
         description=("List of biomarkers attributes."),
     )
-    medical_exams = graphene.Field(
-        graphene.List(graphene.Int),
+    medical_exams = graphene.List(
+        graphene.Int,
         description=("List of medical exams attributes."),
     )
+    prices = graphene.List(
+        SearchProductChannelPrice,
+        description=("Product prices per channel."),
+    )
+    product_type = graphene.Field(SearchProductType, description="Product type info")
 
     class Meta:
         model = product_models.Product
@@ -60,6 +77,26 @@ class SearchProduct(ModelObjectType[product_models.Product]):
             SelectedAttributesByProductIdLoader(info.context)
             .load(root.id)
             .then(lambda attributes: cls._get_attributes(attributes, "medical_exams"))
+        )
+
+    @classmethod
+    def resolve_prices(cls, root: product_models.Product, info):
+        return [
+            SearchProductChannelPrice(
+                channel=p["channel__slug"],
+                amount=float(p["price_amount"]),
+                currency=p["currency"],
+            )
+            for p in root.current_variant_prices
+        ]
+
+    @classmethod
+    def resolve_product_type(cls, root: product_models.Product, info):
+        return SearchProductType(
+            # id=graphene.Node.to_global_id("ProductType", root.product_type.pk),
+            id=root.product_type.pk,
+            name=root.product_type.name,
+            slug=root.product_type.slug,
         )
 
 
